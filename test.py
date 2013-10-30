9dc4f6012b6c306989a8b6e5298831c036b7e79c
kepgor <hectorjrmarco@hotmail.com>
2013-10-30 16:28:26 +0100

    Comentario B Binario Decimal y Test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import DecimalBinario
 numero=int(raw_input("Introduce el número decimal a convertir a binario: "))
 print(DecimalBinario.Decimal2Binario(numero))
+#Comentario: De Binario a Decimal
 str=raw_input("Introduce el número binario a convertir a decimal: ")
 print(DecimalBinario.Decimal2Binario(str))
